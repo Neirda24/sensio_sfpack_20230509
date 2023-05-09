0333c88ab1310e8e65d9e1ceffbbfc0ba1e2a34a
Adrien Roches <adrien@roc-it.tech>
2023-05-09 12:12:13 +0200

    [sfstart/01-controller] Hello world

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    private const REQUIREMENT_NAME_REGEX = '\w+';
+
+    #[Route(
+        '/hello/{name}',
+        name: 'app_hello',
+        requirements: [
+            'name' => self::REQUIREMENT_NAME_REGEX,
+        ],
+        methods: ['GET']
+    )]
+    public function index(?string $name = 'Adrien'): Response
+    {
+        return new Response(
+            content: <<<"HTML"
+            Hello {$name} !
+            HTML
+        );
+    }
+}
