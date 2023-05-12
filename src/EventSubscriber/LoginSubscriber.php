302d12dfc0af7b64d198b2f8f0226d247664daba
Adrien Roches <adrien@roc-it.tech>
2023-05-12 14:44:00 +0200

    [sfmaster/04-events] Improve listener to use event's User

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -5,30 +5,15 @@ namespace App\EventSubscriber;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Psr\Clock\ClockInterface;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly Security $security,
         private readonly ClockInterface $clock,
         private readonly UserRepository $userRepository,
     ) {
-    }
-
-    public function updateLastLoggedIn(LoginSuccessEvent $event): void
-    {
-        $user = $this->security->getUser();
-
-        if (!$user instanceof User) {
-            return;
-        }
-
-        $user->setLastLoggedIn($this->clock->now());
-
-        $this->userRepository->save($user, true);
     }
 
     public static function getSubscribedEvents(): array
@@ -38,5 +23,18 @@ class LoginSubscriber implements EventSubscriberInterface
                 ['updateLastLoggedIn', 0]
             ],
         ];
+    }
+
+    public function updateLastLoggedIn(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setLastLoggedIn($this->clock->now());
+
+        $this->userRepository->save($user, true);
     }
 }
